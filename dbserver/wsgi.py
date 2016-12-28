6c60a8ee1e192781c3dbc61020e05fe99e16d3bc
burmistrovm <burmistrowm@mail.ru>
2016-12-29 02:07:44 +0300

    Раскоммитил related2

--- dbserver/wsgi.py
+++ dbserver/wsgi.py
@@ -6,14 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
-
 import os
 import sys
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, ".."))) 
 
-settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+root_path = os.path.abspath(os.path.split(__file__)[0])
+sys.path.insert(0, os.path.join(root_path, 'project_name'))
+sys.path.insert(0, root_path)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
